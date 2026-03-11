03b2d98fe8dcfcdaf549eab39daed2a8f8f3a030
psuryawanshi <psuryawanshi@LT2408HZA185>
2026-03-11 15:33:11 +0530

    implemented dbcontext & sql migration

--- TaskManagementAPI/Data/ApplicationDbContext.cs
+++ TaskManagementAPI/Data/ApplicationDbContext.cs
@@ -12,5 +12,28 @@ namespace TaskManagementAPI.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<TaskItem> Tasks { get; set; }
+
+        //model builder
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+            //this is fluent API syntax
+            base.OnModelCreating(modelBuilder);
+
+            //configure user-task relationship
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.tasks)
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.userId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //configure indexes for better performance
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+        }
     }
 }
